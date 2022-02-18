c1bb8f7cfc9f962d1146107b95c04fdebea92b65
米鸿坤 <1045262335@qq.com>
2022-02-18 09:03:18 +0000

    x

--- bank.cpp
+++ bank.cpp
@@ -33,12 +33,61 @@ int main() {
 	min_per_cust = MIN_PER_HR / perhour;
 
 	Item temp; // new customer data
+	long turnaways = 0; // turned away by full queue
+	long customers = 0; // joined the queue
+	long served = 0; // served during the simulation
+	long sum_line = 0; // cumulative line length
+	int wait_time = 0; // time util autoteller is free
+	long line_wait = 0; // cumulative time in line 
 
+	// running the simulation
+	for (int cycle = 0; cycle < cyclelimit; cycle ++) {
+		if (newcustomer(min_per_cust)) { // have newcomer
+			if (line.isfull()) {
+				turnaways++;
+			} else {
+				customers++;
+				temp.set(cycle);
+				line.enqueue(temp);
+			}
+		}
+		if (wait_time <= 0 && !line.isempty()) {
+			line.dequeue(temp);
+			wait_time = temp.ptime();
+			line_wait += cycle - temp.when();
+			served++;
+		}
+		if (wait_time > 0) {
+			wait_time--;
+		}
+		sum_line += line.queuecount();
+	}
 
-
-
-
-
+	// reporting results
+	if (customers > 0) {
+		cout << "customers accepted: " << customers << endl;
+		cout << "  customers served: " << served << endl;
+		cout << "         turnaways: " << turnaways << endl;
+		cout << "average queue size: ";
+		cout.precision(2);
+		cout.setf(ios_base::fixed, ios_base::floatfield);
+		cout << (double)sum_line / cyclelimit << endl;
+		cout << " average wait time: "
+			<< (double)line_wait / served << " minutes." << endl;
+	} else {
+		cout << "No customers!" << endl;
+	}
+	cout << "Done!" << endl;
 
 	return 0;
 }
+
+// x= average time, in minutes, between customers
+// return value is ture if customer shows up this minutes
+bool newcustomer(double x) {
+	return (rand() * x / RAND_MAX < 1);
+}
+
+
+
+
