3ed6ac2a770e2610ac09e70f22bc0ecf4383416c
米鸿坤 <1045262335@qq.com>
2022-02-25 06:36:52 +0000

    x

--- fowl2.cpp
+++ fowl2.cpp
@@ -13,7 +13,7 @@
 //		unique_ptr<string>(new string("Goose Eggs")),
 //	};
 //	unique_ptr<string> pwin;
-//	pwin = films[2]; // films[2] loses ownership
+//	pwin = move(films[2]); // films[2] loses ownership
 //
 //	cout << "The nominees for best avian baseball film are" << endl;
 //	for (int i = 0; i < 5; i++) {
